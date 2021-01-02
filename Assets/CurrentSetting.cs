b94be92bdc80b6baf31492d269a9ecf323ea806d
Charles <clombardo169@gmail.com>
2021-01-02 00:42:26 -0500

    Minor 1AM tweaks
    +Users can now see what setting is applied in the main menu
    +Stat pickups are now on a layer below everything else
    +You can now resize the game window

---
+++ Assets/CurrentSetting.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CurrentSetting : MonoBehaviour
+{
+    public TextMeshProUGUI settingText;
+    private int currentSettingIndex;
+    private int pastSettingIndex;
+
+    void Update()
+    {
+        currentSettingIndex = QualitySettings.GetQualityLevel();
+        if(currentSettingIndex != pastSettingIndex)
+            UpdateSettingsText(currentSettingIndex);
+        pastSettingIndex = currentSettingIndex;
+    }
+
+    void UpdateSettingsText(int _index)
+    {
+        switch(_index)
+        {
+            case 0:
+                settingText.text = "Current - Very Low";
+                break;
+            case 1:
+                settingText.text = "Current - Low";
+                break;
+            case 2:
+                settingText.text = "Current - Medium";
+                break;
+            case 3:
+                settingText.text = "Current - High";
+                break;
+            case 4:
+                settingText.text = "Current - Very High";
+                break;
+            case 5:
+                settingText.text = "Current - Ultra";
+                break;
+        }
+    }
+}
