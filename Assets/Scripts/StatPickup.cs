c915acd837f3e3a5755095f06ca7071c593ae03f
Charles <clombardo169@gmail.com>
2021-01-01 19:54:03 -0500

    More pickup work
    +New pickup/powerup sounds
    +Pickup animation completed
    +Changed some quality settings to reduce dependency on pixel lighting

--- Assets/Scripts/StatPickup.cs
+++ Assets/Scripts/StatPickup.cs
@@ -8,7 +8,18 @@ public class StatPickup : MonoBehaviour
 {
     [SerializeField] private PickupType pickupType;
     [SerializeField] private float amount = 0f;
+    public AudioClip pickupClip;
+    private AudioSource audioPlayer;
+    private ParticleSystem pickupAnimation;
+    private SpriteRenderer sprite;
     
+    void Start()
+    {
+        pickupAnimation = GetComponent<ParticleSystem>();
+        sprite = GetComponent<SpriteRenderer>();
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
     void OnTriggerEnter2D(Collider2D _collider)
     {
         Take(_collider.GetComponent<PlayerManager>());
@@ -19,12 +30,20 @@ public class StatPickup : MonoBehaviour
         if(pickupType == PickupType.health && _player.health < _player.maxHealth)
         {
             _player.Heal(amount);
-            Destroy(gameObject);
+            PickedUp();
         }
         else if(pickupType == PickupType.melee && _player.meleePower < _player.maxMeleePower)
         {
             _player.Recharge(amount);
-            Destroy(gameObject);
+            PickedUp();
         }
+    }
+
+    void PickedUp()
+    {
+        audioPlayer.PlayOneShot(pickupClip);
+        pickupAnimation.Play();
+        sprite.enabled = false;
+        Destroy(gameObject, 2f);
     }
 }
